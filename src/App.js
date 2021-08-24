9746dbd9b93ab8adfde93f195b8bb70da49f6414
Nathan Fedzen <nathanfedzen@gmail.com>
2021-08-24 16:42:02 -0600

    added gif and styled button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   
   return (
+    
     <div className="App">
+      <h1>Random Anime Quotes</h1>
       <Quote />
     </div>
   );
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,21 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import "../App.css";
 
+export default function Quote() {
+  const [quote, setQuote] = useState("");
 
-export default function Quote(){
-  const [quote, setQuote] = useState('')
-  
-  function fetchQuote(){
-    fetch('https://animechan.vercel.app/api/random')
-      .then(res => res.json())
-      .then(q => setQuote(q))
+  function fetchQuote() {
+    fetch("https://animechan.vercel.app/api/random")
+      .then((res) => res.json())
+      .then((q) => setQuote(q));
   }
 
-
-  return(
-    <div id='quote-container'>
-      <h1>Your random anime quote: </h1>
-      <h2>{quote.quote}</h2>
-      <button onClick={fetchQuote}>Get Random Anime Quote</button>
-    </div>
-  )
+  if (quote) {
+    return (
+      <div>
+        <button class="myButton" onClick={fetchQuote}>Get Another Random Anime Quote</button>
+        <div id="quote-container">
+          <h2>{quote.quote} </h2>
+          <h3>
+            -{quote.character} ({quote.anime})
+          </h3>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button class="myButton" onClick={fetchQuote}>Get Random Anime Quote</button>
+        <div id="quote-container">
+          <iframe
+            title="naruto"
+            src="https://giphy.com/embed/Mj0gk1wnekXC0"
+            width="480"
+            height="360"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    );
+  }
 }
